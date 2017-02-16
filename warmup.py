fcc5a5b16130d20888a575157d7e654f67b4927c
Ian Price <michael.ian.price@gmail.com>
2017-02-16 10:53:14 -0500

    Circle drawn, no movement yet

--- warmup.py
+++ warmup.py
@@ -1,11 +1,34 @@
 import pygame
 
+width = 500
+height = 500
+
+class Circle(object):
+    def __init__(self,screen,radius,position,color,direction):
+        self.screen = screen
+        self.radius = radius
+        self.position = position
+        self.color = color
+        self.direction = direction
+
+    def draw(self):
+        pygame.draw.circle(self.screen, self.color, self.position, self.radius)
+
+    def updateDirection(self):
+        for i,v in enumerate(self.direction):
+            if self.position[i] + self.radius > width or self.position[i] + self.radius > height or self.position[i] - self.radius < 0:
+                v = -v
+
+    def updatePosition(self):
+        for i,v in enumerate(self.position):
+            v += self.direction[i]
+
 
 
 def main():
-    width = 500
-    height = 500
     blue_color = (97, 159, 182)
+    white_color = (0,0,0)
+
     color_array = []
     '''
     CREATE ALL COLORS
@@ -17,38 +40,44 @@ def main():
     print len(color_array)
     '''
 
-
+    # Game initialization
     pygame.init()
     screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption('My Color Show')
+    pygame.display.set_caption('Warmup')
     clock = pygame.time.Clock()
 
-    # Game initialization
+    # Create circle to move
+    circle = Circle(screen,25,(250,250),blue_color,(5,5))
+
 
     stop_game = False
     while not stop_game:
         for event in pygame.event.get():
 
             # Event handling
-
             if event.type == pygame.QUIT:
                 stop_game = True
 
 
         # Game logic
+        circle.updatePosition()
+        circle.updateDirection()
 
         # Draw background
-
+        screen.fill(white_color)
+        circle.draw()
         '''
         PRINT ALL COLORS, ONE EACH TIME THROUGH THE LOOP
         screen.fill(color_array[color_pointer])
+        color_pointer += 1
         '''
 
         # Game display
 
         pygame.display.update()
         clock.tick(1)
-        color_pointer += 1
+
+
 
     pygame.quit()
 
